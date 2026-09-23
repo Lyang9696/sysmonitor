11e1ad60187d0b18b9c2b245d72142530f2df7f8
Lyang9696 <126447826+Lyang9696@users.noreply.github.com>
2026-09-23 10:40:11 +0800

    chore: 打包脚本 rebuild_admin.bat 纳入版本管理

---
+++ builder/rebuild_admin.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d C:\Users\EDY\PycharmProjects\sysmonitor
+echo [1/3] pyinstaller onedir...
+python -m PyInstaller --noconfirm --windowed --uac-admin --name sysmonitor --icon assets\icon.ico --add-data "assets\app_icon.png;." --paths src --hidden-import win32timezone --hidden-import wmi --hidden-import win32com.client --hidden-import PySide6.QtNetwork src\main.py > build_log.txt 2>&1
+echo [2/3] pyinstaller onefile...
+python -m PyInstaller --noconfirm --windowed --uac-admin --name sysmonitor-portable --icon assets\icon.ico --add-data "assets\app_icon.png;." --paths src --hidden-import win32timezone --hidden-import wmi --hidden-import win32com.client --hidden-import PySide6.QtNetwork --onefile src\main.py >> build_log.txt 2>&1
+echo [3/3] inno build...
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" builder\setup.iss >> build_log.txt 2>&1
+echo ALL_DONE >> build_log.txt
